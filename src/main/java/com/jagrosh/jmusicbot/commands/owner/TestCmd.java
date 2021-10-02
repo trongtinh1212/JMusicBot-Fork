f5ce3e5bf820d73f78937447a2d1cb714412e814
d1m0s23 <61113540+d1m0s23@users.noreply.github.com>
2021-10-02 23:28:32 +0300

    ваще сибался отсюда

--- src/main/java/com/jagrosh/jmusicbot/commands/owner/TestCmd.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/owner/TestCmd.java
@@ -2,17 +2,16 @@ package com.jagrosh.jmusicbot.commands.owner;
 
 import com.jagrosh.jdautilities.command.CommandEvent;
 import com.jagrosh.jmusicbot.commands.OwnerCommand;
-import com.jagrosh.jmusicbot.lang.Lang;
 
-import java.io.IOException;
-
-public class TestCmd extends OwnerCommand {
-    public TestCmd() {
-        this.name = "test";
-    }
-    @Override
-    protected void execute(CommandEvent event) {
-        Lang lang = new Lang(event.getGuild().getIdLong());
-        event.reply(lang.getString("xui"));
-    }
-}
+//import java.io.IOException;
+//
+//public class TestCmd extends OwnerCommand {
+//    public TestCmd() {
+//        this.name = "test";
+//    }
+//    @Override
+//    protected void execute(CommandEvent event) {
+//        Lang lang = new Lang(event.getGuild().getIdLong());
+//        event.reply(lang.getString("xui"));
+//    }
+//}
