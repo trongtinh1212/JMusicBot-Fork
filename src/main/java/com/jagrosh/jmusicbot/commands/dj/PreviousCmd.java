e1f0425c6daf3b91b0cb1fd2c45624425cd9b01e
d1m0s23 <61113540+d1m0s23@users.noreply.github.com>
2021-10-03 18:14:54 +0300

    previous fix

--- src/main/java/com/jagrosh/jmusicbot/commands/dj/PreviousCmd.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/dj/PreviousCmd.java
@@ -5,29 +5,38 @@ import com.jagrosh.jmusicbot.Bot;
 import com.jagrosh.jmusicbot.audio.AudioHandler;
 import com.jagrosh.jmusicbot.audio.QueuedTrack;
 import com.jagrosh.jmusicbot.commands.DJCommand;
+import net.dv8tion.jda.api.entities.GuildVoiceState;
 
 public class PreviousCmd extends DJCommand {
     public PreviousCmd(Bot bot) {
         super(bot);
         this.name = "previous";
         this.help = "plays the previous track";
+        this.aliases = bot.getConfig().getAliases(this.name);
     }
 
     @Override
     public void doCommand(CommandEvent event) {
         AudioHandler handler = (AudioHandler)event.getGuild().getAudioManager().getSendingHandler();
+        GuildVoiceState userState = event.getMember().getVoiceState();
 
         if(handler.getPreviousTrack() != null) {
             if(handler.getNowPlaying(bot.getJDA()) != null) {
                 try {
-                    handler.getPlayer().getPlayingTrack().stop();
-                    handler.getQueue().addAt(0, new QueuedTrack(handler.getPreviousTrack().makeClone(), handler.getPreviousTrack().getUserData(Long.class) == null ? 0L : handler.getPreviousTrack().getUserData(Long.class)));
+                    handler.getQueue().add(new QueuedTrack(handler.getPreviousTrack().makeClone(), handler.getPreviousTrack().getUserData(Long.class) == null ? 0L :handler.getPreviousTrack().getUserData(Long.class)));
+                    handler.getPlayer().stopTrack();
                 } finally {
                     event.replySuccess("Previous loaded!");
                 }
             } else {
                 try {
-                    handler.getPlayer().playTrack(handler.getPreviousTrack().makeClone());
+                    if(userState.inVoiceChannel()) {
+                        event.getGuild().getAudioManager().openAudioConnection(userState.getChannel());
+                        handler.getQueue().add(new QueuedTrack(handler.getPreviousTrack().makeClone(), handler.getPreviousTrack().getUserData(Long.class) == null ? 0L :handler.getPreviousTrack().getUserData(Long.class)));
+                        handler.getPlayer().stopTrack();
+                    } else {
+                        event.replyError("You not in voice channel!");
+                    }
                 } finally {
                     event.replySuccess("Previous loaded!");
                 }
